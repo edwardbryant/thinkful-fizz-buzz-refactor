aea97b6ffbcae42c0aac7c93b5a7136428af1171
edwardbryant <edward.bryant@gmail.com>
2015-01-05 15:02:39 -0600

    finish

--- app.js
+++ app.js
@@ -1,52 +1,29 @@
-
 $(document).ready(function(){
-    $('#guts').text(" Counting ... ");
-    for (var i=1;i<=100;i++){
-
-        if (checkForFizz(i)) {
-            if (checkForBuzz(i)) {
+    $('#user-submit').on('click', function() {
+        var user_range = $('#user-range').val();
+        user_range = parseInt(user_range,10);
+        fizzbuzz(user_range);
+    });        
+})
+function fizzbuzz(num){
+    $('#guts').text(" Counting to "+num+" ... ");
+    for (var i=1;i<=num;i++){
+        if (i % 3 == 0) {
+            if (i % 5 == 0) {
                 var x = '<span style="color:red;font-weight:bold;">fizz-buzz</span>';
             } else {
                 var x = '<span style="color:red;">fizz</span>';
             }
-        } else if (checkForBuzz(i)) {
+        } else if (i % 5 == 0) {
             var x = '<span style="color:red;">buzz</span>';    
         } else {
             var x = i;
         }
-
-        if (i == 100) {
+        if (i == num) {
             var node = $('<span>'+x+'.</span>'); 
         } else {
             var node = $('<span>'+x+', </span>'); 
         }
-
         $('#guts').append(node);
-    }
-})
-
-function checkForFizz(num) {
-    if (num % 3 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function checkForBuzz(num) {
-    if (num % 5 == 0) {
-        return true;
-    } else {
-        return false;
-    }    
-}
-
-function checkForFizzBuzz(num) {
-    var a = checkForFizz(num);
-    var b = checkForBuzz(num);
-    if (a == true || b == true) {
-        return true;
-    } else {
-        return false;
     }
 }
